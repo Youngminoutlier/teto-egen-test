3b67ebb00f44ad142e18d422927164395789f878
Youngminoutlier <tnsb5373@gmail.com>
2025-06-17 17:22:15 +0900

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,9 +3,15 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  base: '/',
   build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
     rollupOptions: {
       input: 'public/index.html'
     }
+  },
+  define: {
+    'process.env': {}
   }
 })
